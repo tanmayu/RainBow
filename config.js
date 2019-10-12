36087de63e0f1ea678447c4c1349db5ba90869ef
Tanmay Ratnawat <52318709+tanmayu@users.noreply.github.com>
2019-10-12 23:25:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-      token: "<SECRET>",
+      token: "NjMyNTYzNTczMDI1MTQ0ODUy.XaIIjA.RS8Wv-gVAK927P8GIgt1u9WGv44",
       hiz: 3000,
-      role_id: "486616559083847690",
-      sunucu_id: "480044387540271104",     
+      role_id: "615103212151635968",
+      sunucu_id: "615077286521208843",     
 }
