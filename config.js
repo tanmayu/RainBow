a6e5868f706c9dea4ce1ba213a655434ed28689b
Tanmay Ratnawat <52318709+tanmayu@users.noreply.github.com>
2019-10-13 10:04:59 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-      token: "NjMyNTYzNTczMDI1MTQ0ODUy.XaIIjA.RS8Wv-gVAK927P8GIgt1u9WGv44",
-      hiz: 3000,
-      role_id: "615103212151635968",
-      sunucu_id: "615077286521208843",     
-}
